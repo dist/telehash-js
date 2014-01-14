2bf265586367ba0849b9d2134480ffc8bb15d994
doppelganger <mail.jiby.me@gmail.com>
2014-01-15 00:28:29 +0530

    cleanup and better npm scripts

--- build.js
+++ build.js
@@ -30,10 +30,12 @@ b.bundle().pipe(bundle1);
 
 bundle1.on('finish', function () {
 	fs.truncateSync(bundle, 0);
+	fs.truncateSync('./examples/browser/js/bundle.js', 0);
 	var data = UglifyJS.minify(includes);
 	// temp hack
 	data.code = data.code.replace('"use strict";','');
 	fs.appendFileSync(bundle, data.code);
+	fs.appendFileSync('./examples/browser/js/bundle.js', data.code);
 	fs.unlink('./build/bundle1.js');
 });
 
--- seeds.js
+++ seeds.js
@@ -1,10 +1,12 @@
-var seeds = [{
-    "ip": "111.92.110.221",
-    "port": 42424,
-    "ip6": "fe80::22c9:d0ff:fe78:df3b",
-    "port6": 42424,
-    "hashname": "bbad8b51a29ae1b14bbc7f5e1e8ef485ffa21782500e04808269bdc380222f76",
-    "pubkey": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwvZulzCwbNZ5CPUNNV42\npkTTHDfxa2jAXtZlqdRevFY7PGgk53lvH1lOlrw/u7SxRWVHTtX235AFJ//KdY9+\ns1UAQWFE7R+oDPZujRiBG3imWcz/niITumwKi6Hz7+jOTz3ymnJ+R2SX3dMeyHGF\ngx8+8rqLD2yDv9rZgqglvGnd1IwcolMC8jqnyBuJIYubu3MoMbIdCtduXWpR4a8V\nQ8GnuysfuM0Om6uHQoQJXHmtZT84QluZlCJ1x0wgdb9DtotzVA6YxiATFMrmKK5U\nHSZ3ubb5lk+2VvyXcv/Xx2nRzzv+Gw/6FqWvLnQkMUDSaPYK7xc5w9Y4vJIBUK2M\nfwIDAQAB\n-----END PUBLIC KEY-----\n",
-    "http": "http://192.168.0.10:42424",
-    "bridge": true
-}]
+var seeds = [
+    {
+        "ip": "192.168.0.10",
+        "port": 42424,
+        "ip6": "fe80::22c9:d0ff:fe78:df3b",
+        "port6": 42424,
+        "hashname": "dd1a8765ff03eb3816a620fcf4e94419a961c54ef0f50b81842256e2a935854d",
+        "pubkey": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8IXNQ7Am4ske/3UObgIa\nrhFd6yI6wYjjIRQRRsa57D9FQfWuT3iSX8uXqjhyoRwhqmhn27QBQucloCeki6MX\n431Lh4Tp3ziqnFnf2dvXL2Zop9Yzff0WPkTdkBa+50KdSFvwYZRCKIxhlShAGNqc\nImUSTmCEz0AIH2HSQMY0RfQ1OzSZUm8+BDfObZ+81fxMnzSqh2hTjcQKyz9x0ar4\nZ9vpV0DLrnrd3mc9SDf3nN+7qH2CoIxhqzTv545daQ9PPtc3mvd7a4ta4XeWvRyO\nRNRlknOB7597NUEoSDxjVLBGsLjXOOiVVdv9i1jDdVE0esqjUmjYw6I+iOebGYSN\nEwIDAQAB\n-----END PUBLIC KEY-----\n",
+        "http": "http://192.168.0.10:42424",
+        "bridge": true
+    }
+]
